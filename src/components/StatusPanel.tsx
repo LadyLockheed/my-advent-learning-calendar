48b0a2d3fb46cd80b389f93be2a72f05d3e39d36
Karin Karlsson <karin.drake@gmail.com>
2024-11-27 17:22:05 +0100

    Add StatusPanel boilerplate

---
+++ src/components/StatusPanel.tsx
@@ -0,0 +1,6 @@
+import styles from './statusPanel.module.scss';
+
+const StatusPanel = () => {
+	return <div className={styles.statusPanelContainer}>StatusPanel</div>;
+};
+export default StatusPanel;
