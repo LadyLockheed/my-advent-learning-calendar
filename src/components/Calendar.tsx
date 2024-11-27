274773fa66e96289990ad0682e9e34537f34a219
Karin Karlsson <karin.drake@gmail.com>
2024-11-27 17:21:52 +0100

    Add calendar component boilerplate

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,10 @@
+import styles from './calendar.module.scss';
+
+const Calendar = () => {
+	return (
+		<div className={styles.calendarContainer}>
+			<div className={styles.calendar}>Calendar</div>
+		</div>
+	);
+};
+export default Calendar;
