1f18a44d391e1f2c7c32bbc7fc7541a8bb59a0d5
Karin Karlsson <karin.drake@gmail.com>
2024-11-28 12:51:26 +0100

    Create and implement function for validation if door can open

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,9 +2,17 @@ import styles from './calendar.module.scss';
 import Door from './Door';
 
 const Calendar = () => {
+	//the doors can only be opened from start december, and only if doors date is less than or equal to todays date
+	const validateDateOfDoor = (doorNumber: number): boolean => {
+		const todaysDate = new Date();
+		const isDecember = todaysDate.getMonth() + 1 === 12;
+
+		return isDecember && doorNumber <= todaysDate.getDate();
+	};
+
 	const doors = Array.from({ length: 24 }, (_, index) => ({
 		isOpen: false,
-		isUnlocked: false,
+		isUnlocked: validateDateOfDoor(index + 1),
 		doorNumber: index + 1, // Door numbers start from 1
 	}));
 
